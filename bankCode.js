0bb6e7464d2608db7da320ff1a34623437d7a398
klonggg <49969240+klonggg@users.noreply.github.com>
2022-06-29 09:09:16 +0700

    Add files via upload

---
+++ bankCode.js
@@ -0,0 +1,277 @@
+export const listBank = [
+  {
+    code: '970436',
+    value: 'Vietcombank_NH TMCP Ngoai thuong VN (VCB)',
+    keyword: ['vc', 'vcb', 'vietcom', 'vietcombank', 'vc bank']
+  },
+  {
+    code: '970418',
+    value: 'BIDV_NH TMCP Dau tu và Phat trien VN',
+    keyword: ['bidv', 'bi dv']
+  },
+  {
+    code: '970405',
+    value: 'Agribank_NH Nong nghiep và Phat trien nong thon Việt Nam',
+    keyword: ['agr', 'agri', 'nong nghiep', 'nông nghiệp', 'nông thôn', 'nong thon', 'agribank']
+  },
+  {
+    code: '970422',
+    value: 'MB_NH TMCP Quan Doi',
+    keyword: ['mb', 'mb bank', 'mbb', 'mbbank', 'mb_nh']
+  },
+  {
+    code: '970416',
+    value: 'ACB_NH TMCP A Chau',
+    keyword: ['acb', 'acb bank', 'abc', 'a chau', 'á châu', 'abc bank']
+  },
+  {
+    code: '970425',
+    value: 'ABB_NH TMCP An Binh (ABBank)',
+    keyword: ['abb']
+  },
+  {
+    code: '801011',
+    value: 'NHBHN_NONGHYUP - Chi nhanh HN',
+    keyword: ['nonghyup']
+  },
+  {
+    code: '970400',
+    value: 'Saigonbank_NH TMCP Sai Gon Cong Thuong (SGB)',
+    keyword: ['saigon', 'sgb', 'sai gon', 'sai gon bank', 'saigonbank']
+  },
+  {
+    code: '970403',
+    value: 'SacomBank_NH TMCP Sai gon Thuong Tin (STB)',
+    keyword: ['sacom', 'sacombank']
+  },
+  {
+    code: '970406',
+    value: 'DONGABANK_NH TMCP Dong A (DAB)',
+    keyword: ['dong a', 'đông á', 'donga', 'da b', 'dab', 'dongabank']
+  },
+  {
+    code: '970407',
+    value: 'Techcombank_NH TMCP Ky thuong Viet Nam (TCB)',
+    keyword: ['tech', 'techcom', 'techcombank', 'tcb', 'tcbank', 'tc bank', 'tc b']
+  },
+  {
+    code: '970408',
+    value: 'GPBank_NHTM TNHH MTV Dau Khi Toan Cau (GPB)',
+    keyword: ['gbb', 'gbbank']
+  },
+  {
+    code: '970409',
+    value: 'BAB_ NH TMCP Bac A',
+    keyword: ['bac a', 'baca', 'bacabank', 'bab', 'babank', 'ba bank']
+  },
+  {
+    code: '970410',
+    value: 'SCVN_NH TNHH MTV STANDARD CHARTERED BANK LIMITED (Viet Nam)',
+    keyword: ['sc', 'scvn']
+  },
+  {
+    code: '970412',
+    value: 'PVcomBank_NH TMCP Dai Chung Viet Nam',
+    keyword: ['pv', 'pvcom', 'pvcombank', 'pv com bank']
+  },
+  {
+    code: '970414',
+    value: 'OceanBank_NH TM TNHH MTV Dai Duong',
+    keyword: ['ocean', 'oceanbank', 'dai duong', 'đại dương']
+  },
+  {
+    code: '970419',
+    value: 'NCB_NH TMCP Quoc Dan',
+    keyword: ['ncb', 'quocdan', 'quoc dan', 'quốc dân']
+  },
+  {
+    code: '970421',
+    value: 'VRB_NH Lien doanh Viet Nga',
+    keyword: ['vrb', 'vr']
+  },
+  {
+    code: '970423',
+    value: 'TPBank_NH TMCP Tien Phong (TPB)',
+    keyword: ['tp', 'tpbank', 'tp bank', 'tp b', 'tienphong', 'tien phong', 'tiền phong']
+  },
+  {
+    code: '970424',
+    value: 'SHBVN_Ngan hang TNHH MTV ShinHan Viet Nam',
+    keyword: ['shin', 'shinhan', 'shin han']
+  },
+  {
+    code: '970426',
+    value: 'MSB_NH TMCP Hang Hai',
+    keyword: ['msb', 'msbb', 'msbbank', 'msb bank']
+  },
+  {
+    code: '970427',
+    value: 'VietABank_NH TMCP Viet A',
+    keyword: ['vieta', 'vab', 'viet a', 'việt á', 'vietabank']
+  },
+  {
+    code: '970428',
+    value: 'NAMABANK_ NH TMCP Nam A',
+    keyword: ['nama', 'nam a', 'nam á', 'namabank', 'nam á bank']
+  },
+  {
+    code: '970429',
+    value: 'SCB_NH TMCP Sai Gon',
+    keyword: ['scb']
+  },
+  {
+    code: '970430',
+    value: 'PGBank_NH TMCP Xang Dau Petrolimex',
+    keyword: ['pg', 'pgbank', 'pg bank']
+  },
+  {
+    code: '970431',
+    value: 'Eximbank_NH TMCP Xuat nhap khau VN (EIB)',
+    keyword: ['exim', 'eximbank', 'exim bank']
+  },
+  {
+    code: '970432',
+    value: 'VPB_NH TMCP Viet Nam Thinh Vuong',
+    keyword: ['vb', 'vbbank', 'vb bank', 'vbb']
+  },
+  {
+    code: '970433',
+    value: 'VietBank_NH TMCP Viet Nam Thuong Tin',
+    keyword: ['vietbank', 'viet bank']
+  },
+  {
+    code: '970434',
+    value: 'INDOVINABANK_NH TNHH Indovina (IVB)',
+    keyword: ['indovinabank', 'indovina', 'ivb']
+  },
+  {
+    code: '970437',
+    value: 'HDBank_NH TMCP Phat Trien TP Ho Chi Minh (HDB)',
+    keyword: ['hd', 'hdb', 'hd bank', 'hdbank']
+  },
+  {
+    code: '970438',
+    value: 'BaoVietBank_NH TMCP Bao Viet (BVB)',
+    keyword: ['baoviet', 'bảo việt', 'bv', 'bvb', 'baovietbank']
+  },
+  {
+    code: '970439',
+    value: 'PBVN_NH TNHH MTV Public Viet Nam (VIDPublic Bank)',
+    keyword: ['pvvn', 'vid', 'vidpublic']
+  },
+  {
+    code: '970440',
+    value: 'SEABANK_NH TMCP Dong Nam A',
+    keyword: ['sea', 'seabank', 'sea bank', 'seab']
+  },
+  {
+    code: '970441',
+    value: 'VIB_NH TMCP Quoc te Viet Nam',
+    keyword: ['vib', 'vib bank', 'vibbank']
+  },
+  {
+    code: '970442',
+    value: 'HONGLEONGBANK_NH TNHH MTV Hong Leong Viet Nam (HLBVN)',
+    keyword: ['hongleongbank']
+  },
+  {
+    code: '970443',
+    value: 'SHB_NH TMCP Sai Gon Ha Noi',
+    keyword: ['shb', 'shb bank', 'shbank']
+  },
+  {
+    code: '970444',
+    value: 'CBB_NH TM TNHH MTV XAY DUNG VIET NAM',
+    keyword: ['cbb', 'cbbbank', 'cbb bank']
+  },
+  {
+    code: '970446',
+    value: 'COOPBANK_Ngan hang Hop tac xa Viet Nam',
+    keyword: ['coopbank', 'co op', 'coop']
+  },
+  {
+    code: '970448',
+    value: 'OCB_NH TMCP Phuong Dong',
+    keyword: ['ocb', 'ocbbank', 'ocbank']
+  },
+  {
+    code: '970449',
+    value: 'LPB_NH TMCP Buu Dien Lien Viet (LienVietPostBank)',
+    keyword: ['lienviet', 'lienvietpost', 'lienvietpostbank', 'lpb', 'lienvietbank']
+  },
+  {
+    code: '970452',
+    value: 'Kienlongbank_NH TMCP Kien Long (KLB)',
+    keyword: ['kienlong', 'kienlongbank', 'klb']
+  },
+  {
+    code: '970454',
+    value: 'VietCapitalBank_NH TMCP Ban Viet',
+    keyword: ['vietcapital', 'vietcapitalbank', 'ban viet', 'banviet']
+  },
+  {
+    code: '970455',
+    value: 'IBK_Industrial Bank of Korea - CN Ha Noi',
+    keyword: ['ibk hn']
+  },
+  {
+    code: '970456',
+    value: 'IBK_Industrial Bank of Korea - CN Ho Chi Minh',
+    keyword: ['ibk hcm']
+  },
+  {
+    code: '970457',
+    value: 'Wooribank_NH TNHH MTV Woori Viet Nam',
+    keyword: ['wooribank', 'woori']
+  },
+  {
+    code: '970458',
+    value: 'UOB_NH TNHH MTV United Overseas(VietNam)',
+    keyword: ['uob', 'overseas']
+  },
+  {
+    code: '970462',
+    value: 'KBHN_Ngan hang Kookmin Bank chi nhanh Ha Noi',
+    keyword: ['kookmin', 'kookminbank', 'kbhn']
+  },
+  {
+    code: '796500',
+    value: 'DBS_Ngan hang DBS - chi nhanh Thanh pho Ho Chi Minh',
+    keyword: ['dbs']
+  },
+  {
+    code: '668888',
+    value: 'KBank_NH Dai chung TNHH Kasikornbank - CN TP HCM',
+    keyword: ['kbank', 'kbbank']
+  },
+  {
+    code: '546035',
+    value: 'UBANK by VPBANK_NH TMCP Viet Nam Thinh Vuong',
+    keyword: ['ubank', 'ub']
+  },
+  {
+    code: '546034',
+    value: 'CAKE by VPBANK_NH TMCP Viet Nam Thinh Vuong',
+    keyword: ['cake']
+  },
+  {
+    code: '970463',
+    value: 'KBHCM_Ngan hang Kookmin Bank chi nhanh Ho Chi Minh',
+    keyword: ['kbhcm']
+  },
+  {
+    code: '422589',
+    value: 'CIMB_NH TNHH MTV CIMB',
+    keyword: ['cimb']
+  },
+  {
+    code: '458761',
+    value: 'HSBC_NH TNHH MTV HSBC (Viet Nam)',
+    keyword: ['hsbc']
+  },
+  {
+    code: '970415',
+    value: 'Vietinbank TMCP Cong Thuong',
+    keyword: ['vietin', 'viettin', 'vietinbank', 'viettinbank', 'vietin bank', 'viettin bank']
+  }
+]
